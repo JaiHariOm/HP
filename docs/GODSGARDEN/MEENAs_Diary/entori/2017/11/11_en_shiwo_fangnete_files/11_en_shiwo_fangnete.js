ee44fb36a65e3104ffeee0f1fee927c872d29f73
JaiHariOm <andromeda@ANDROMEDA.local>
2019-03-02 13:56:38 +0900

    fix: Change text entry method in Diary
    Diaryのテキストの記入方法を変更

--- docs/GODSGARDEN/MEENAs_Diary/entori/2017/11/11_en_shiwo_fangnete_files/11_en_shiwo_fangnete.js
+++ docs/GODSGARDEN/MEENAs_Diary/entori/2017/11/11_en_shiwo_fangnete_files/11_en_shiwo_fangnete.js
@@ -4,4 +4,4 @@ setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('11_en_shiwo_fangnete_files/11_en_shiwo_fangneteMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
