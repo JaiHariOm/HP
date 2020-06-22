0870817b2a951cfad3c8841c3bec7583bbe285f3
JaiHariOm <jaijaihariom@yahoo.com>
2020-06-22 15:28:06 +0900

    :+1: Subject 再調整
    クリティカルなバグ修正
    test3 SparkleでのTest

---
+++ docs/js/Contents/MEENA'sPhotoAlubum/Album_1.20200622152128.js
@@ -0,0 +1,29 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js261').attr('src', (dpi>1) ? '../../images/dscn0206-1-664.jpg' : '../../images/dscn0206-1-332.jpg');
+$('.js262').attr('src', (dpi>1) ? '../../images/dscn0220-650.jpg' : '../../images/dscn0220-325.jpg');
+$('.js263').attr('src', (dpi>1) ? '../../images/imeji-2-630.jpg' : '../../images/imeji-2-315.jpg');
+$('.js264').attr('src', (dpi>1) ? '../../images/dscn9793-392.jpg' : '../../images/dscn9793-196.jpg');
+$('.js265').attr('src', (dpi>1) ? '../../images/hua-xiang-20-1-530.jpg' : '../../images/hua-xiang-20-1-265.jpg');
+$('.js266').attr('src', (dpi>1) ? '../../images/dscn0234-2-682.jpg' : '../../images/dscn0234-2-341.jpg');
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+
+});
