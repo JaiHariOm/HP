68c2276a44e27bf4369b3b556fff2f2d352c2bdc
JaiHariOm <jaijaihariom@yahoo.com>
2020-06-24 16:19:12 +0900

    :tada: Subject ファイル構成の変更
    chenge: Sparkleの仕様かリンクがうまく作動しなかったので
    ファイル構成の変更をした。

---
+++ docs/js/harishwelcome.20200624160656.js
@@ -0,0 +1,23 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js623').attr('src', (dpi>1) ? 'images/facebook-158.png' : 'images/facebook-79.png');
+$('.js624').attr('src', (dpi>1) ? 'images/tumblr-158.png' : 'images/tumblr-79.png');
+
+
+
+
+
+
+
+
+
+
+
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+
+});
